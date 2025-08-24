537c83a90cf34193f61eeac706202c1c8b93e103
CarlosCondor <jc@bipicar.com>
2025-08-24 19:29:13 +0200

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://USERNAME.github.io',
-  base: '/carloscondor-landing',
+  site: 'https://www.carloscondor.com',
+  // base: '/carloscondor-landing', // Solo para GitHub Pages
 });
